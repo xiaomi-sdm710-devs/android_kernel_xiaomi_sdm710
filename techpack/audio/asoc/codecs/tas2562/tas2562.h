55d1786c3624d06ec3a7c44cc3b4b86f7d1cb830
Ivan Vecera <ivan@cera.cz>
2022-12-02 18:31:23 +0100

    tas2562: Remove unused structures and struct members in tas2562_priv
    Signed-off-by: Ivan Vecera <ivan@cera.cz>
    Change-Id: Ia074233dd9acd4965d354af969be673f95e04ec8

--- techpack/audio/asoc/codecs/tas2562/tas2562.h
+++ techpack/audio/asoc/codecs/tas2562/tas2562.h
@@ -576,17 +576,6 @@ TAS2562_InterruptConfiguration_PININTConfig10_Assert2msOnLatchedInterrupts \
 #define ERROR_I2C_SUSPEND           -1
 #define ERROR_I2C_FAILED            -2
 
-struct tas2562_register {
-int book;
-int page;
-int reg;
-};
-
-struct tas2562_dai_cfg {
-unsigned int dai_fmt;
-unsigned int tdm_delay;
-};
-
 struct tas2562_priv {
 struct device *dev;
 struct regmap *regmap;
@@ -597,22 +586,16 @@ struct hrtimer mtimer;
 #ifdef CONFIG_TAS2562_CODEC
 struct snd_soc_codec *codec;
 #endif
-int mnClkin;
-int mnClkid;
 bool mbPowerUp;
 int mnPowerState;
 int mnCurrentBook;
 int mnCurrentPage;
-int mnLoad;
 int mnASIFormat;
 int mnResetGPIO;
 int mnIRQGPIO;
 int mnIRQ;
 bool mbIRQEnable;
 int mnSamplingRate;
-int mnFrameSize;
-int mnPLL;
-int mnPPG;
 int mnCh_size;
 int mnSlot_width;
 int mnPCMFormat;
