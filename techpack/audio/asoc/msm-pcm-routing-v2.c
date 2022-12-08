0ff4ae04a0cbd5c555337dd63207db17fcf1e4d2
Ivan Vecera <ivan@cera.cz>
2022-12-08 17:10:52 +0100

    techpack: audio: Remove unnecessary tas2562-calib
    tas2562-calib allows an userspace to read & write calibration
    data using /dev/tas_calib device but there is no such userspace
    program that would require this device. Remove it.
    
    Signed-off-by: Ivan Vecera <ivan@cera.cz>
    Change-Id: Ic73dcbe929bd925eae10045c6986c71219b716ef

--- techpack/audio/asoc/msm-pcm-routing-v2.c
+++ techpack/audio/asoc/msm-pcm-routing-v2.c
@@ -49,8 +49,6 @@
 #include "msm-ds2-dap-config.h"
 #if IS_ENABLED(CONFIG_MACH_XIAOMI_PYXIS_OR_VELA)
 #include "dsp/smart_amp.h"
-
-#include "tas2562-calib.h"
 #endif
 #ifndef CONFIG_DOLBY_DAP
 #undef DOLBY_ADM_COPP_TOPOLOGY_ID
@@ -19771,19 +19769,12 @@ static int __init msm_soc_routing_platform_init(void)
 	memset(&be_dai_name_table, 0, sizeof(be_dai_name_table));
 	memset(&last_be_id_configured, 0, sizeof(last_be_id_configured));
 
-#if IS_ENABLED(CONFIG_MACH_XIAOMI_PYXIS_OR_VELA)
-	tas_calib_init();
-#endif
-
 	return platform_driver_register(&msm_routing_pcm_driver);
 }
 module_init(msm_soc_routing_platform_init);
 
 static void __exit msm_soc_routing_platform_exit(void)
 {
-#if IS_ENABLED(CONFIG_MACH_XIAOMI_PYXIS_OR_VELA)
-	tas_calib_exit();
-#endif
 	msm_routing_delete_cal_data();
 	memset(&be_dai_name_table, 0, sizeof(be_dai_name_table));
 	mutex_destroy(&routing_lock);
